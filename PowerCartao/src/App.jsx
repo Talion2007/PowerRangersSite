ec2f864509a33c7e694a760b35de5754c9f89464
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2025-04-16 20:07:57 -0300

    Versão Funcionando

--- PowerCartao/src/App.jsx
+++ PowerCartao/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Home from './Home';
 import Dados from './Dados';
 import SeuPower from './SeuPower';
 
 function App() {
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -13,6 +16,9 @@ function App() {
         <Route path="/seu-power" element={<SeuPower />} />
       </Routes>
     </Router>
+    <Analytics />
+    <SpeedInsights />
+    </>
   );
 }
 
